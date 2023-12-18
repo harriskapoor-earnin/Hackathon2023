f0209354b80292f465a3113c38c7d6bb5f962378
harriskapoor-earnin <harris.kapoor@earnin.com>
2023-12-18 12:20:15 -0800

    Initial Commit

---
+++ Hackathon2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hackathon2023
+//
+//  Created by Harris Kapoor on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hackathon2023/Hackathon2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon2023App.swift
+//  Hackathon2023
+//
+//  Created by Harris Kapoor on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
